ebdca5c156aed318f36c9abe9c7ee06eacf2790b
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2025-08-29 09:26:47 +0530

    homepage initialized

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import bodyParser from "body-parser";
 import ejs from "ejs";
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
 app.get("/", (req,res)=>{
     res.render("home.ejs");
 })
